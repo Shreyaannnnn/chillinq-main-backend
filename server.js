29eeddd2b1de22ebcf16c63baddc2dedb8fa4a87
Shreyaannnnn <58767697+Shreyaannnnn@users.noreply.github.com>
2023-11-03 19:11:39 +0530

    Update server.js serverport

--- server.js
+++ server.js
@@ -249,7 +249,7 @@ async function startServer() {
       });
     });
 
-    server.listen(3000, () => {
+    server.listen(process.env.PORT, () => {
       console.log('Server is running on port 3000');
     });
   } catch (error) {
